774adfc8e74641bbd14ffa224ccdf6bd2466a8fb
Mark Lux <marlx6590@163.com>
2016-09-21 20:44:37 +0800

    基本功能完成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,15 @@
 @inject('helper','App\ViewComposer\ViewHelper')
 
 @section('content')
-    <div class="container">
-        <div class="header-bg">
+    <div class="header" style="background-image: url(http://i1.piimg.com/567571/4b170449b0ab8f93.jpg)">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="logotxt"><h1><a href="http://marklux.cn">Mark Lux's Pub</a></h1></div>
+                    <h2 class="site-name text-center">故事与酒<span></span></h2>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap -->
     <!-- 新 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+    <link rel="stylesheet" href="{{url('/')}}/css/site.min.css">
     <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
     <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
