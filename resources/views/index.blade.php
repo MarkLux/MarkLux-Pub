9c5a4873652305b41beef9c57d03ca00cc9895ca
MarkLux <marlx6590@163.com>
2017-12-20 17:34:36 +0800

    adjust index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 @inject('helper','App\ViewComposer\ViewHelper')
 
 @section('content')
-    <div class="header" style="background-image: url(http://p1.bqimg.com/567571/36610bce391f91a4.jpg)">
+    <div class="header" style="background-image: url(http://of1deuret.bkt.clouddn.com/17-11-21/7862004.jpg); height:768px">
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
                     <div class="logotxt"><h1><a href="http://marklux.cn">Mark Lux's Pub</a></h1></div>
-                    <h2 class="site-name text-center">静待花开<span></span></h2>
+                    <h2 class="site-name text-center">无风叶落<span></span></h2>
                 </div>
             </div>
 
