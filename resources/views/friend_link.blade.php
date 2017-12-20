9a269460a9aa16ed38c8a2a4177dff8f7e8d1a97
MarkLux <marlx6590@163.com>
2017-12-20 17:54:38 +0800

    adjust about page

--- resources/views/friend_link.blade.php
+++ resources/views/friend_link.blade.php
@@ -2,20 +2,22 @@
 @section('content')
     <div class="container" id="friend_links">
     <h3>酒馆常客</h3>
-    <li>
-        <ul><a href="http://helloyz.cn/">Molybdenumyz</a></ul>
-        <ul><a href="http://ihotown.cn/">Hotown</a></ul>
-        <ul><a href="http://outxu.cn">Out Xu</a></ul>
-        <ul><a href="http://mypub.sealbaby.cn/">MariaSeal</a></ul>
-        <ul><a href="http://catango.cn/">Catango</a></ul>
-    </li>
+    <ul>
+        <li><a href="http://helloyz.cn/">Molybdenumyz</a></li>
+        <li><a href="http://ihotown.cn/">Hotown</a></li>
+        <li><a href="http://outxu.cn">Out Xu</a></li>
+        <li><a href="http://mypub.sealbaby.cn/">MariaSeal</a></li>
+        <li><a href="http://catango.cn/">Catango</a></li>
+    </ul>
     <h3>关于我</h3>
     <p>
         非典型码农。大三CS挣扎期。
         <br>
         啥都干不好但却怀着啥都想干的好奇心和热情orz
         <br>
-        同性交友请点击-> <a href="https://github.com/MarkLux">github</a>
+        自我定位不是程序员，是一个兴趣使然的创作爱好者
+        <br>
+        作品集 -> <a href="https://github.com/MarkLux">github</a>
     </p>
     </div>
 @endsection
